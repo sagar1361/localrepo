2dbc32f6f852d1304cd6ea3ce9195c5c78abc2cf
sagar1361 <chouhansagar1306@gmail.com>
2023-10-19 18:40:09 +0530

    New file Added

--- changes.js
+++ changes.js
@@ -1,1 +1,7 @@
-console.log((document.getElementById("header-title"));
+// console.log(document.getElementById("header-title"));
+var Title = document.getElementById("header-title");
+// Title.textContent = "Hello";
+// Title.innerText ="Hey";
+Title.style.borderBottom = "solid 3px #000";
+var AddItem = document.getElementById("main");
+AddItem.innerText;
