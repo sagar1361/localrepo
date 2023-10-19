ee9bdb7fa3007155a547cec59540ba3f6f09e07f
sagar1361 <chouhansagar1306@gmail.com>
2023-10-19 19:26:10 +0530

    some changes Added

--- changes.js
+++ changes.js
@@ -1,7 +1,16 @@
 // console.log(document.getElementById("header-title"));
-var Title = document.getElementById("header-title");
-// Title.textContent = "Hello";
-// Title.innerText ="Hey";
-Title.style.borderBottom = "solid 3px #000";
-var AddItem = document.getElementById("main");
-AddItem.innerText;
+// var Title = document.getElementById("header-title");
+// // Title.textContent = "Hello";
+// // Title.innerText ="Hey";
+// Title.style.borderBottom = "solid 3px #000";
+// var AddItem = document.getElementById("main");
+// AddItem.innerText;
+
+
+// document.getElementsByClassName
+var items = document.getElementsByClassName("list-group-item");
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight = "bold";
+}
+items[2].style.backgroundColor = "green";
+
