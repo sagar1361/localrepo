f6f979e0f78f38373714ac928c6f1dc8f5325c40
sagar1361 <chouhansagar1306@gmail.com>
2023-10-20 17:56:14 +0530

    getElementByTagName

--- changes.js
+++ changes.js
@@ -1,16 +1,24 @@
 // console.log(document.getElementById("header-title"));
 // var Title = document.getElementById("header-title");
-// // Title.textContent = "Hello";
-// // Title.innerText ="Hey";
+// Title.textContent = "Hello";
+// Title.innerText ="Hey";
 // Title.style.borderBottom = "solid 3px #000";
 // var AddItem = document.getElementById("main");
 // AddItem.innerText;
 
 
 // document.getElementsByClassName
-var items = document.getElementsByClassName("list-group-item");
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight = "bold";
-}
-items[2].style.backgroundColor = "green";
+// var items = document.getElementsByClassName("list-group-item");
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight = "bold";
+// }
+// items[2].style.backgroundColor = "green";
 
+// document.getElementsByTagName
+var List = document.getElementsByTagName("li");
+for(var i=0;i<List.length;i++){
+    List[i].style.fontWeight = "bold";
+}
+
+List[2].style.backgroundColor = "green";
+List[4].style.color = "red";
