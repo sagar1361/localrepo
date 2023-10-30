5372c2aab892ebf23f5fecc205bfe2906d757ddf
sagar1361 <chouhansagar1306@gmail.com>
2023-10-30 19:00:32 +0530

    Filter Added

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var form = document.getElementById("addForm");
 var Addlist = document.getElementById("items");
+var filter  = document.getElementById("filter");
 
 // Submit Event
 form.addEventListener("submit", addItem);
@@ -7,12 +8,14 @@ form.addEventListener("submit", addItem);
 function addItem(e){
     e.preventDefault();
     var newItem = document.getElementById("item").value;
+    var newdesp = document.getElementById("description").value;
     // creating new li Element
     var list = document.createElement("li");
 
     list.className = "list-group-item";
     // Adding text node
     list.appendChild(document.createTextNode(newItem));
+    list.appendChild(document.createTextNode(newdesp));
     // Creating delete button
     var delbtn = document.createElement("button");
     delbtn.className = "btn btn-danger btn-sm float-right delete";
@@ -22,7 +25,7 @@ function addItem(e){
     // Append textnode
     delbtn.appendChild(document.createTextNode(" X"));
 
-    Editbtn.appendChild(document.createTextNode("+"));
+    Editbtn.appendChild(document.createTextNode("Edit"));
     // appent btn to list
     list.appendChild(delbtn);
     list.appendChild(Editbtn);
@@ -39,4 +42,25 @@ function removeItem(e){
             Addlist.removeChild(list);
         }
     }
+}
+
+// Filter Event
+filter.addEventListener("keyup", filterItems);
+
+function filterItems(e){
+    e.preventDefault(); 
+    // convert text to Lowercase
+    var lcase = e.target.value.toLowerCase();
+    // get list
+    var items = Addlist.getElementsByTagName("li");
+    // convert to an Array
+    Array.from(items).forEach(function(list){
+        var itemName = list.firstChild.textContent + list.childNodes[1].textContent;
+        if(itemName.toLowerCase().indexOf(lcase) != -1){
+            list.style.display = "block";
+        } 
+        else{
+            list.style.display = "none";
+        }
+    });
 }
